e7f1c313e4794506c1f9faaee2a0e6fd75cc6f4a
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-22 22:03:14 -0500

    Updated documentation signatures.

--- src/features/code-review/recommendedFix.ts
+++ src/features/code-review/recommendedFix.ts
@@ -1,14 +1,14 @@
-/*
+/**
  * Represents a recommended fix for an issue.
  */
 export interface RecommendedFix
 {
-    /*
+    /**
      * The description of the fix.
      */
     description: string;
     
-    /*
+    /**
      * An example of how to apply the fix.
      */
     example: string;
