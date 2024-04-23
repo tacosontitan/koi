ada6130d98d85b08e7205bedce579c2186074605
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-22 22:45:56 -0500

    Created core request and response models.

--- src/request.ts
+++ src/request.ts
@@ -1,7 +1,7 @@
 /*
  * Represents a request within the context of the extension.
  */
-export interface Request
+export interface Request<T>
 {
     /**
      * The ID of the request.
---
+++ src/response.ts
@@ -0,0 +1,10 @@
+/**
+ * Represents a response to a request within the context of the extension.
+ */
+export interface Response
+{
+    /**
+     * The ID of the request the response is associated with.
+     */
+    id: string;
+}
